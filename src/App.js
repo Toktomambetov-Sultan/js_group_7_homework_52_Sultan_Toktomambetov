56fcba57ae4cca3c5f6bce961c821aeee97a9872
Toktomambetov Sultan <sultan.toktomambetov.04@bk.ru>
2020-07-18 15:40:06 +0600

    did level4, it remains to finish the interface

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Card from "./components/Card";
+import Card, { translator } from "./components/Card";
 import './assets/cards.css'
 function random(max) {
   return Math.floor(Math.random() * max);
 }
 class CardDeck {
   constructor(state) {
-    this.deck = this.updateNewDeck();
+    this.updateNewDeck();
   }
   updateNewDeck() {
     const cards = [];
@@ -17,7 +17,7 @@ class CardDeck {
         });
       }
     }
-    return cards;
+    this.deck = cards;
   }
   _getCard() {
     return this.deck.splice(random(this.deck.length), 1);
@@ -25,8 +25,38 @@ class CardDeck {
   getCards(int = 5) {
     const cards = [];
     for (let i = 0; i < int; i++) { cards.push(this._getCard(cards)[0]); }
-    console.log(cards);
     return cards;
+  }
+}
+class PokerHandMeneger {
+  funcs = [
+    cards => new Set(cards.map(card => card.suit)).size == 1 ? "flush" : false,
+    function (cards) {
+      const ranks = cards.map(card => card.rank);
+      const copyRanks = [...ranks];
+      const result = {};
+      ranks.forEach((rank) => {
+        let repeat = 0;
+        while (copyRanks.indexOf(rank) + 1) {
+          repeat++;
+          copyRanks.splice(copyRanks.indexOf(rank), 1);
+        }
+        if (repeat) {
+          result[rank] = repeat;
+        }
+      })
+      const entResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
+      if (entResult[0][1] >= 3) { return "three of a kind " + translator.rank[entResult[0][0]] }
+      else if (entResult[0][1] === 2 && entResult[1][1] === 2) { return "two pairs " + translator.rank[entResult[0][0]] + ":" + translator.rank[entResult[1][0]] }
+      else if (entResult[0][1] === 2) { return "one pair " + translator.rank[entResult[0][0]] }
+      else { return "ничего" }
+    }
+  ]
+  setCards(cards) {
+    const CopyCards = [...cards];
+    this.funcs.forEach((func) => {
+      if (func(CopyCards)) { console.log(func(CopyCards)); }
+    })
   }
 }
 class App extends Component {
@@ -36,20 +66,22 @@ class App extends Component {
       cards: [],
     }
     this.deck = new CardDeck();
+    this.PokerHandMeneger = new PokerHandMeneger();
   }
   generate = () => {
-    const cards = [...this.state.cards, ...this.deck.getCards()];
+    const cards = [...this.deck.getCards()];
+    this.PokerHandMeneger.setCards(cards);
     this.setState({ cards });
+    this.deck.updateNewDeck();
   }
   render() {
     return (
       <div>
-        <button onClick={this.generate}>generate</button>
+        <button onClick={this.generate} >generate</button>
         <div className="playingCards">
           {this.state.cards.map((cards, index) => (
             <Card key={index} suit={cards.suit} rank={cards.rank} />
           ))}
-          
         </div>
       </div>
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,14 @@ const translator = {
     suit: [["diams", "♦"], ["hearts", "♥"], ["clubs", "♣"], ["spades", "♠"]],
     rank: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 }
-
 function Card(props) {
-    console.log(props);
     return (
         <div className={"card" + " " + "rank-" + translator.rank[props.rank].toLowerCase()
-                + " " + translator.suit[props.suit][0]}>
+            + " " + translator.suit[props.suit][0]}>
             <span className="rank">{translator.rank[props.rank]}</span>
             <span className="suit">{translator.suit[props.suit][1]}</span>
         </div>
     )
 }
+export {translator}
 export default Card
