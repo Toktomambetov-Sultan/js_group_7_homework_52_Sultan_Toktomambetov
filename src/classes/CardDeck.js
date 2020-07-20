7ac5b658fa5152abea8dea63f0c123bd5a277908
Toktomambetov Sultan <sultan.toktomambetov.04@bk.ru>
2020-07-20 12:27:40 +0600

    wrote algorithms for all combinations

--- src/classes/CardDeck.js
+++ src/classes/CardDeck.js
@@ -1,25 +1,25 @@
-import {random} from "./../assets/MathFunctions";
+import { random } from "./../assets/MathFunctions";
 export class CardDeck {
-    constructor() {
-      this.updateNewDeck();
-    }
-    updateNewDeck() {
-      const cards = [];
-      for (let rank = 0; rank < 13; rank++) {
-        for (let suit = 0; suit < 4; suit++) {
-          cards.push({
-            rank, suit
-          });
-        }
-      }
-      this.deck = cards;
-    }
-    _getCard() {
-      return this.deck.splice(random(this.deck.length), 1);
-    }
-    getCards(int = 5) {
-      const cards = [];
-      for (let i = 0; i < int; i++) { cards.push(this._getCard(cards)[0]); }
-      return cards;
-    }
+  constructor() {
+    this.updateNewDeck();
   }
+  updateNewDeck() {
+    let cards = [];
+    for (let rank = 0; rank < 13; rank++) {
+      for (let suit = 0; suit < 4; suit++) {
+        cards.push({
+          rank, suit
+        });
+      }
+    }
+    this.deck = cards;
+  }
+  _getCard() {
+    return this.deck.splice(random(this.deck.length), 1);
+  }
+  getCards(int = 5) {
+    const cards = [];
+    for (let i = 0; i < int; i++) { cards.push(this._getCard(cards)[0]); }
+    return cards;
+  }
+}
--- src/classes/PokerHandManager.js
+++ src/classes/PokerHandManager.js
@@ -1,25 +1,95 @@
-import {translator} from "./../assets/trasnlator";
-console.log(translator);
+import { translator } from "./../assets/trasnlator";
 export class PokerHandMeneger {
+  arrayShift(arr, k) {
+    for (let i = 0; i < k; i++) arr.unshift(arr.pop());
+    return arr
+  }
+  getReps(cards) {
+    const ranks = cards.map(card => card.rank);
+    const result = [];
+    ranks.forEach((rank, index) => {
+      let indexes = [];
+      ranks.forEach((newRank, newIndex) => {
+        if (newRank === rank && !(ranks.slice(index + 1).indexOf(rank) + 1)) {
+          indexes.push(newIndex);
+        }
+      })
+      if (indexes.length) { result.push({ rank, indexes }); }
+    })
+    return result.sort((a, b) => b.indexes.length - a.indexes.length);
+  }
+  isOrder(arr) {
+    let newArr = [...arr];
+    let allRanks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    function GetJsonAns(arr) { return JSON.stringify(arr).slice(1, -1) };
+    for (let i = 0; i < newArr.length; i++) {
+      for (let k = 0; k < allRanks.length; k++) {
+        if (GetJsonAns(allRanks).indexOf(GetJsonAns(newArr)) + 1) {
+          return true;
+        }
+        allRanks = this.arrayShift(allRanks, 1);
+      }
+      newArr = this.arrayShift(newArr, 1);
+    }
+    return false;
+  }
   funcs = [
+    cards => {
+      if (new Set(cards.map(card => card.suit)).size === 1) {
+        const ranks = cards.map(card=>card.rank).sort((a,b)=>a-b);
+        if(ranks[0]===8 && ranks[ranks.length-1]==12 && this.isOrder(ranks)){
+          return{
+            winning: "Royal flush",
+            indexes: [0,1,2,3,4],
+          }
+        }
+      }
+    },
+    cards => {
+      if (new Set(cards.map(card => card.suit)).size === 1
+        && this.isOrder(cards.map(card => card.rank).sort((a, b) => a - b))) {
+        return {
+          winning: "straight flush",
+          indexes: [0, 1, 2, 3, 4],
+        }
+      }
+    },
+    cards => {
+      const entResult = this.getReps(cards);
+      if (entResult[0].indexes.length === 4) {
+        return {
+          winning: "four of a kind " + translator.rank[entResult[0].rank],
+          indexes: [...entResult[0].indexes],
+        }
+      }
+    },
+    cards => {
+      const entResult = this.getReps(cards);
+      if (entResult[0].indexes.length === 3 && entResult[1].indexes.length == 2) {
+        return {
+          winning: "full house " + translator.rank[entResult[0].rank] + " : " + translator.rank[entResult[1].rank],
+          indexes: [...entResult[0].indexes, ...entResult[1].indexes],
+        }
+      }
+    },
     cards => new Set(cards.map(card => card.suit)).size === 1 ? {
       winning: "flush " + translator.suit[cards[0].suit][1],
       indexes: [0, 1, 2, 3, 4],
     } : false,
-    function (cards) {
-      const ranks = cards.map(card => card.rank);
-      const result = [];
-      ranks.forEach((rank, index) => {
-        let indexes = [];
-        ranks.forEach((newRank, newIndex) => {
-          if (newRank === rank && !(ranks.slice(index + 1).indexOf(rank) + 1)) {
-            indexes.push(newIndex);
-          }
-        })
-        if (indexes.length) { result.push({ rank, indexes }); }
-      })
-      const entResult = result.sort((a, b) => b.indexes.length - a.indexes.length);
-      if (entResult[0].indexes.length >= 3) {
+    cards => {
+      let ranks = cards.map(card => card.rank).sort((a, b) => a - b);
+      if (this.isOrder(ranks)) {
+        return {
+          winning: "straight",
+          indexes: [0, 1, 2, 3, 4],
+        }
+      } else {
+        return false;
+      }
+    },
+    (cards) => {
+      const entResult = this.getReps(cards);
+      if (entResult[0].indexes.length === 3) {
         return {
           winning: "three of kind " + translator.rank[entResult[0].rank],
           indexes: [...entResult[0].indexes],
@@ -27,7 +97,7 @@ export class PokerHandMeneger {
       }
       else if (entResult[0].indexes.length === 2 && entResult[1].indexes.length === 2) {
         return {
-          winning: "two pairs " + translator.rank[entResult[0].rank] + ":" + translator.rank[entResult[1].rank],
+          winning: "two pairs " + translator.rank[entResult[0].rank] + " : " + translator.rank[entResult[1].rank],
           indexes: [...entResult[0].indexes, ...entResult[1].indexes],
         }
       }
